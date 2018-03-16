e01617df92d56ac4d8afb01eb8377e073f8c6331
ssusanu <sergio.susanu.developer@gmail.com>
2018-03-16 13:24:33 +0200

    Exercitii 1 Java Core

--- Ex1Square/src/SquarePackage/main.java
+++ Ex1Square/src/SquarePackage/main.java
@@ -1,5 +1,10 @@
 package SquarePackage;
 
+import Circle.Circle;
+import Student.Student;
+
+import java.sql.SQLOutput;
+
 public class main {
 
     public static void main(String[] args) {
@@ -7,5 +12,20 @@ public class main {
         Patrat myPatrat= new Patrat(5);
         System.out.println(myPatrat.calculateArea());
         System.out.println(myPatrat.calculatePerimeter());
+
+        Circle myCircle= new Circle(10);
+        System.out.println(myCircle.getArea());
+        System.out.println(myCircle.getPerimeter());
+
+        Student Vasea = new Student();
+        Vasea.setData("Vasea", 20);
+
+        Student Feghea = new Student();
+        Feghea.setData("Feghea",30);
+
+        System.out.println(Vasea.getAge());
+        System.out.println(Feghea.getAge());
+
+        System.out.println("Average age: " + (Vasea.getAge() + Feghea.getAge())/2);
     }
 }
